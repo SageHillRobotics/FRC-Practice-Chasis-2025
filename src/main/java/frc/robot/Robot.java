344b0c06b201e159cb2f35e77219f96a13c027b3
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2025-10-20 10:09:34 -0700

    restarted implementation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Swerve;
 
 public class Robot extends TimedRobot {
-  private Swerve m_swerve;
-
   private XboxController m_controller;
 
   public Robot() {
-    m_swerve = new Swerve(1, 2, 3, 4, 5, 6, 7, 8);
-
     m_controller = new XboxController(0);
   }
 
@@ -46,13 +41,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    String motor = "ALL";
-    if (m_controller.getLeftBumperButton()) motor = "FL";
-    if (m_controller.getRightBumperButton()) motor = "FR";
-    if (m_controller.getLeftTriggerAxis() > 0.8) motor = "BL";
-    if (m_controller.getRightTriggerAxis() > 0.8) motor = "BR";
-    m_swerve.move(m_controller.getLeftX(), m_controller.getRightY(), motor);
-    m_swerve.logEncoders();
+    
   }
 
   @Override
