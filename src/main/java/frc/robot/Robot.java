f3accc582c17297478aee93c1a816abe0befaa68
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2025-11-02 08:30:53 -0800

    reset everything

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Swerve;
 
 public class Robot extends TimedRobot {
-  private Swerve m_swerve = new Swerve(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
   private XboxController m_controller = new XboxController(0);
   private Timer m_timer = new Timer();
 
@@ -42,12 +40,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    String motor = "ALL";
-    if (m_controller.getLeftBumperButton()) motor = "FL";
-    if (m_controller.getRightBumperButton()) motor = "FR";
-    if (m_controller.getLeftTriggerAxis() > 0.8) motor = "BL";
-    if (m_controller.getRightTriggerAxis() > 0.8) motor = "BR";
-    m_swerve.move(m_controller.getRightX(), -m_controller.getLeftY(), motor);
+    
   }
 
   @Override
