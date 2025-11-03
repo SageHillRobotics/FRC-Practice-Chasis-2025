c0d0e510201930a7cf53b276691b21b2af34b4ea
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2025-11-03 15:28:09 -0800

    added pathplanner

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -10,10 +10,10 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveDrive {
     SwerveModule[] swerveModules = new SwerveModule[]{
-        new SwerveModule(1, 1, 2), // Front left
-        new SwerveModule(2, 3, 4), // Front right
-        new SwerveModule(3, 5, 6), // Back left
-        new SwerveModule(4, 7, 8)  // Back right
+        new SwerveModule(1, 1, 2),
+        new SwerveModule(2, 3, 4),
+        new SwerveModule(3, 5, 6),
+        new SwerveModule(4, 7, 8)
     };
 
     public void drive(double x1, double y1, double x2) {
@@ -22,10 +22,10 @@ public class SwerveDrive {
 
     public void drive(ChassisSpeeds chassisSpeeds) {
         SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
-            new Translation2d(0.5, 0.5), // Front left
-            new Translation2d(0.5, -0.5), // Front right
-            new Translation2d(-0.5, 0.5), // Back left
-            new Translation2d(-0.5, -0.5) // Back right
+            new Translation2d(0.5, 0.5),
+            new Translation2d(0.5, -0.5),
+            new Translation2d(-0.5, 0.5),
+            new Translation2d(-0.5, -0.5)
         );
         SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(chassisSpeeds);
         for (int i = 0; i < 4; i++) {
