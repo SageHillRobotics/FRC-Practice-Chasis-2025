e8391578f181a4a8b7f1f616d430a38195a5388e
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2025-12-12 14:28:03 -0800

    pose2d

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,12 +1,19 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotation;
+
 import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsystem extends SubsystemBase {
@@ -30,8 +37,40 @@ public class SwerveSubsystem extends SubsystemBase {
         new Translation2d(-CHASSIS_LENGTH / 2, CHASSIS_WIDTH / 2)
     );
 
+    private SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, new Rotation2d(), new SwerveModulePosition[]{
+        new SwerveModulePosition(swerveModules[0].getDrivePosition(), Rotation2d.fromRadians(swerveModules[0].getSteerPosition())),
+        new SwerveModulePosition(swerveModules[1].getDrivePosition(), Rotation2d.fromRadians(swerveModules[1].getSteerPosition())),
+        new SwerveModulePosition(swerveModules[2].getDrivePosition(), Rotation2d.fromRadians(swerveModules[2].getSteerPosition())),
+        new SwerveModulePosition(swerveModules[3].getDrivePosition(), Rotation2d.fromRadians(swerveModules[3].getSteerPosition()))
+    });
+
     public SwerveSubsystem() {
         pigeon.reset();
+    }
+
+    @Override
+    public void periodic() {
+        odometry.update(Rotation2d.fromRadians(getHeading()), new SwerveModulePosition[]{
+            new SwerveModulePosition(swerveModules[0].getDrivePosition(), Rotation2d.fromRadians(swerveModules[0].getSteerPosition())),
+            new SwerveModulePosition(swerveModules[1].getDrivePosition(), Rotation2d.fromRadians(swerveModules[1].getSteerPosition())),
+            new SwerveModulePosition(swerveModules[2].getDrivePosition(), Rotation2d.fromRadians(swerveModules[2].getSteerPosition())),
+            new SwerveModulePosition(swerveModules[3].getDrivePosition(), Rotation2d.fromRadians(swerveModules[3].getSteerPosition()))
+        });
+        SmartDashboard.putNumber("Robot Heading", Math.toDegrees(getHeading()));
+        SmartDashboard.putString("Robot Position", getPose().getTranslation().toString());
+    }
+
+    public Pose2d getPose() {
+        return odometry.getPoseMeters();
+    }
+
+    public void resetOdometry(Pose2d pose) {
+        odometry.resetPosition(Rotation2d.fromRadians(getHeading()), new SwerveModulePosition[]{
+            new SwerveModulePosition(swerveModules[0].getDrivePosition(), Rotation2d.fromRadians(swerveModules[0].getSteerPosition())),
+            new SwerveModulePosition(swerveModules[1].getDrivePosition(), Rotation2d.fromRadians(swerveModules[1].getSteerPosition())),
+            new SwerveModulePosition(swerveModules[2].getDrivePosition(), Rotation2d.fromRadians(swerveModules[2].getSteerPosition())),
+            new SwerveModulePosition(swerveModules[3].getDrivePosition(), Rotation2d.fromRadians(swerveModules[3].getSteerPosition()))
+        }, pose);
     }
 
     public SwerveDriveKinematics getKinematics() {
